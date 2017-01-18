fd2627af4baf5461fe24315bec7e08dc164a50ba
Sunny S <3030712@gmail.com>
2017-01-17 22:47:30 -0800

    added sqlite exception

---
+++ include/bits/sqlite_exception.hpp
@@ -0,0 +1,50 @@
+// wrapper around sqlite3
+
+//          Copyright Sundeep S. Sangha 2013 - 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef DATA_PATTERN_BITS_SQLITE_EXCEPTION_HPP
+#define DATA_PATTERN_BITS_SQLITE_EXCEPTION_HPP
+
+#include <stdexcept>
+
+namespace data_pattern_sqlite {
+
+/* sqlite exception */
+struct sqlite_exception : public std::runtime_error {
+  
+explicit
+sqlite_exception (
+  std::string const
+, int
+);
+
+sqlite_exception (
+  sqlite_exception const &
+) = default;
+
+sqlite_exception &
+operator = (
+  sqlite_exception const &
+) = default;
+
+sqlite_exception (
+  sqlite_exception &&
+) = default;
+
+sqlite_exception &
+operator = (
+  sqlite_exception &&
+) = default;
+
+~sqlite_exception() noexcept = default;
+
+std::string const err;
+int const rv;
+}; /* sqlite exception */
+
+} /* data_pattern_sqlite */
+#endif
+
